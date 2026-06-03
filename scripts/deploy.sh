6d5fdc50543e45289e4b7815f7e6438759c1c456
aws vm <mohamedalkad65@gmail.com>
2026-06-03 21:32:54 +0000

    update

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ source ./roles.sh
 source ./s3.sh
 source ./sqs.sh
 source ./sns.sh
-source ./lambda/lambda.sh
+source ./lambda.sh
 source ./config_parametars.sh
 source ./app/launch_template.sh
 source ./app/load_balancer.sh
@@ -18,5 +18,9 @@ source ./dns.sh
 source ./certificate.sh
 source ./app/load_balancer_listener.sh
 source ./app/auto_scaling_group.sh
+source ./pipeline/github.sh
+source ./pipeline/codebuild.sh
+source ./pipeline/codedeploy.sh
+source ./pipeline/codepipeline.sh
 source ./app/sns_subscription.sh
 source ./cloud_front.sh
